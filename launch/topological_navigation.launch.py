51bbb89c80700f720300f5fa271f7c0827c1eab3
Charlie Street <me@charliestreet.net>
2024-10-03 10:48:43 +0100

    Add sim time parameter to launch
    Signed-off-by: Charlie Street <me@charliestreet.net>

--- launch/topological_navigation.launch.py
+++ launch/topological_navigation.launch.py
@@ -20,16 +20,22 @@ def generate_launch_description():
     map_path = LaunchConfiguration("map")
     pose_topic = LaunchConfiguration("pose_topic")
     viz = LaunchConfiguration("viz")
+    use_sim_time = LaunchConfiguration("use_sim_time")
 
     map_arg = DeclareLaunchArgument("map")
     pose_topic_arg = DeclareLaunchArgument("pose_topic", default_value="amcl_pose")
     viz_arg = DeclareLaunchArgument("viz", default_value="true")
+    use_sim_time_arg = DeclareLaunchArgument("use_sim_time", default_value="false")
 
     localisation_node = Node(
         package="topological_navigation",
         executable="topological_localisation",
         name="topological_localisation",
-        parameters=[{"map": map_path}, {"pose_topic": pose_topic}],
+        parameters=[
+            {"map": map_path},
+            {"pose_topic": pose_topic},
+            {"use_sim_time": use_sim_time},
+        ],
         output="screen",
     )
 
@@ -37,18 +43,16 @@ def generate_launch_description():
         package="topological_navigation",
         executable="topological_map_visualiser",
         name="topological_map_visualiser",
-        parameters=[{"map": map_path}],
+        parameters=[{"map": map_path}, {"use_sim_time": use_sim_time}],
         output="screen",
-        condition=launch.conditions.IfCondition(
-            launch.substitutions.LaunchConfiguration("viz")
-        ),
+        condition=launch.conditions.IfCondition(viz),
     )
 
     nav_node = Node(
         package="topological_navigation",
         executable="edge_navigation",
         name="edge_navigation_server",
-        parameters=[{"map": map_path}],
+        parameters=[{"map": map_path}, {"use_sim_time": use_sim_time}],
         output="screen",
     )
 
@@ -57,6 +61,7 @@ def generate_launch_description():
     ld.add_action(map_arg)
     ld.add_action(pose_topic_arg)
     ld.add_action(viz_arg)
+    ld.add_action(use_sim_time_arg)
     ld.add_action(localisation_node)
     ld.add_action(viz_node)
     ld.add_action(nav_node)
